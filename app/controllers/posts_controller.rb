1d52ae44016958c93e7dd782a9fca669718e5ef1
bingo8670 <307286130@qq.com>
2017-06-22 21:18:04 +0800

    add category

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,14 @@ class PostsController < ApplicationController
     @post.save
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.update!( post_params )
+
+    render :json => { :id => @post.id, :message => "ok"}
+  end
+
+
   def destroy
     @post = current_user.posts.find(params[:id])   #只能删除自己的帖子
     @post.destroy
@@ -58,7 +66,7 @@ class PostsController < ApplicationController
   protected
 
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:content, :category_id)
   end
 
 end
